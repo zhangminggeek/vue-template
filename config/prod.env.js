89f43255446185dcc6b2592bdea8c9aca565ba8b
zhangming <qianzm2490@vip.qq.com>
2019-03-18 12:42:41 +0800

    chore: 打包配置优化

--- config/prod.env.js
+++ config/prod.env.js
@@ -1,4 +1,17 @@
 'use strict'
+let assetsPublicPath = '' // 外网静态资源oss地址
+switch (process.env.CONFIG) {
+  case 'inner':
+    assetsPublicPath = '"/"'
+    break
+  case 'release':
+    assetsPublicPath = '"/aa"'
+    break
+  default:
+    assetsPublicPath = '"/"'
+    break
+}
+
 module.exports = {
-  assetsPublicPath: '"/aa"' // 外网静态资源oss地址
+  assetsPublicPath: assetsPublicPath
 }
